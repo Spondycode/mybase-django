aa8ec8ea2019d66bb1358fe356938f101fb66267
Dave Allen <spondicious@me.com>
2023-08-30 22:08:01 +0200

    fixed the nabar and the conditional log and logout

--- users/views.py
+++ users/views.py
@@ -9,12 +9,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created {username}! Go and Log in')
+            messages.success(request, f'Your account has been created {username}! Now Log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 
-def login(request):
-    return render(request, 'users/login.html', {})
+# def login(request):
+#     return render(request, 'users/login.html', {})
